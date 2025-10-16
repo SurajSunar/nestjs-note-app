2fc316d890092337133392c496d34180adbede35
Suraj Sunar <surbhu06@gmail.com>
2025-10-16 11:43:37 +0600

    app: generated migration for note

---
+++ prisma/migrations/20251016043830_add_note_module/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "updated_at" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
